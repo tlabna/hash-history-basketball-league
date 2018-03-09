aedee35fae712acced27b99c59a714e5084924c5
tlabna <tlabna@users.noreply.github.com>
2018-03-08 19:54:22 -0500

    feat: Add initial routes + components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
+import Navbar from './Navbar'
+import Players from './Players'
+import Teams from './Teams'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Component
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+
+          <Route path='/' exact component={Home} />
+          <Route path='/players' component={Players} />
+          <Route path='/teams' component={Teams} />
+        </div>
+      </Router>
     )
   }
 }
