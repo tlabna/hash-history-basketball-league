63930f577d07331b8b6339ab0ca627f45daa7e81
tlabna <tlabna@users.noreply.github.com>
2018-03-12 15:59:31 -0400

    feat: Add Article component & update Articles.js to display article
    -Article component:
      - Used to fetch article data and render children prop

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import Article from './Article'
 import { getTeamsArticles } from '../api'
 
 export default class Articles extends Component {
@@ -33,6 +34,20 @@ export default class Articles extends Component {
             list={teamsArticles}
             {...this.props}
           />
+
+          <Route path={`${url}/:articleId`} render={({ match }) => (
+              <Article articleId={match.params.articleId} teamId={teamId}>
+                {(article) => !article ? <h1>Loading</h1> : (
+                  <div className='panel'>
+                    <article className='article' key={article.id}>
+                      <h1 className='header'>{article.title}</h1>
+                      <p>{article.body}</p>
+                    </article>
+                  </div>
+                  )
+                }
+              </Article>
+            )} />
         </div>
   }
 }
